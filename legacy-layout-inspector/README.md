00bed198f9bf23293fb53f86abe33c8e35b1bf65
pingfangx <pingfangx@pingfangx.com>
2022-06-30 13:29:04 +0800

    [legacy layout inspector] 1.0.0

---
+++ legacy-layout-inspector/README.md
@@ -0,0 +1,6 @@
+# Legacy Layout Inspector
+旧版的布局检测。
+
+## 更新说明
+### v1.0.0
+* Add Legacy Layout Inspector action.
--- legacy-layout-inspector/build.gradle.kts
+++ legacy-layout-inspector/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.pingfangx.plugin"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
