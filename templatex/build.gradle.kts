6249a244c1c50e86cbce6f0fd86ae9f649cef06c
pingfangx <pingfangx@pingfangx.com>
2022-07-08 15:24:13 +0800

    [templates] modify plugin config for samples

--- templatex/build.gradle.kts
+++ templatex/build.gradle.kts
@@ -6,8 +6,9 @@ plugins {
     id("org.jetbrains.intellij") version "1.6.0"
 }
 
+val templatexPluginId = "com.pingfangx.plugin.templatex"
 group = "com.pingfangx.plugin"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -18,6 +19,15 @@ intellij {
     type.set("IC")
 
     plugins.set(listOf("android"))
+}
+
+val localProperties = Properties().apply {
+    val file = File(rootDir, "local.properties")
+    if (file.isFile) {
+        file.inputStream().use {
+            load(it)
+        }
+    }
 }
 
 tasks {
@@ -44,18 +54,64 @@ tasks {
         token.set(System.getenv("PUBLISH_TOKEN"))
     }
     runIde {
-        val properties = Properties().apply {
-            val file = File(rootDir, "local.properties")
-            if (file.isFile) {
-                file.inputStream().use {
-                    load(it)
-                }
-            }
-        }
-        properties.getProperty("StudioRunPath")?.let {
-            logger.lifecycle("config ideDir: $it")
+        localProperties.getProperty("StudioRunPath")?.let {
             ideDir.set(file(it))
+            log("config ideDir = $it")
         }
         jvmArgs("-Xmx1G")
     }
+}
+
+localProperties.getProperty("templates")?.toString()?.let {
+    configTemplates(it)
+} ?: run {
+    sourceSets.getByName("main").resources.srcDir("src/templates-samples/resources") // add samples
+}
+fun configTemplates(templates: String?) {
+    templates ?: return
+    logConfig("templates", templates)
+
+    intellij {
+        pluginName.set("${project.name}-$templates")
+        logConfig("pluginName", pluginName)
+    }
+    sourceSets {
+        getByName("main") {
+            java.setSrcDirs(setOf("src/$templates/kotlin"))
+            resources.setSrcDirs(setOf("src/templates-common/resources")) // set common
+            resources.srcDir("src/$templates/resources") // add flavor specific
+            resources.srcDir("src/templates-$templates/resources") // add flavor specific
+            // resources.exclude("src/main/resources/messages/TemplateXBundle.properties") // exclude messages, not work
+            logConfig("java.srcDirs", java.srcDirs)
+            logConfig("resources.srcDirs", resources.srcDirs)
+        }
+    }
+    tasks {
+        patchPluginXml {
+            pluginId.set("$templatexPluginId.${templates.replace("-", ".")}")
+            logConfig("pluginId", pluginId)
+            pluginDescription.set("<li>$templates</li>A group of templates used by the templatex plugin.")
+            logConfig("pluginDescription", pluginDescription)
+        }
+        withType(Jar::class.java) {
+            archiveBaseName.set(intellij.pluginName)
+            exclude("com/**") // java.setSrcDirs includes src/main/kotlin by default (because of the Kotlin plugin)
+            exclude("**/messages/**") // exclude messages, sourceSets.main.resources.exclude not work
+            exclude("**/META-INF/*.kotlin_module") // exclude .kotlin_module
+        }
+    }
+}
+
+fun logConfig(config: String, any: Any) {
+    log(
+        "config $config = " + if (any is Provider<*>) {
+            any.get()
+        } else {
+            any.toString()
+        }
+    )
+}
+
+fun log(any: Any) {
+    logger.lifecycle(any.toString())
 }
