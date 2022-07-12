7facdcc2d1798cefcf31eb7beaa0354eaa027570
pingfangx <pingfangx@pingfangx.com>
2022-07-12 16:10:10 +0800

    [templatex] 1.0.1 remove renderTemplate

--- templatex/src/main/kotlin/com/pingfangx/plugin/templatex/android/AndroidLayoutResourceFileTemplateCreator.kt
+++ templatex/src/main/kotlin/com/pingfangx/plugin/templatex/android/AndroidLayoutResourceFileTemplateCreator.kt
@@ -7,13 +7,8 @@ import com.android.tools.idea.npw.project.getModuleTemplates
 import com.android.tools.idea.npw.project.getPackageForApplication
 import com.android.tools.idea.npw.project.getPackageForPath
 import com.android.tools.idea.npw.template.TemplateResolver
-import com.android.tools.idea.templates.recipe.DefaultRecipeExecutor
-import com.android.tools.idea.templates.recipe.RenderingContext
-import com.android.tools.idea.wizard.template.ModuleTemplateData
-import com.android.tools.idea.wizard.template.RecipeExecutor
 import com.android.tools.idea.wizard.template.TextFieldWidget
 import com.android.tools.idea.wizard.template.WizardUiContext
-import com.android.tools.idea.wizard.template.impl.other.files.layoutResourceFile.res.layoutXml
 import com.google.wireless.android.sdk.stats.AndroidStudioEvent
 import com.intellij.ide.fileTemplates.FileTemplate
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -24,7 +19,6 @@ import com.intellij.psi.PsiElement
 import com.pingfangx.plugin.templatex.creator.TemplateCreator
 import com.pingfangx.plugin.templatex.util.TemplateXUtils
 import org.jetbrains.android.facet.AndroidFacet
-import java.io.File
 import java.util.*
 
 /**
@@ -100,52 +94,5 @@ class AndroidLayoutResourceFileTemplateCreator(
             e.printStackTrace()
         }
         return null
-    }
-
-    /**
-     * com.android.tools.idea.npw.model.RenderTemplateModel$TemplateRenderer#renderTemplate
-     *
-     * 但是执行时报错 java.lang.NoSuchMethodError: com.android.tools.idea.npw.model.RenderTemplateModel.getModuleTemplateDataBuilder()Lcom/android/tools/idea/templates/ModuleTemplateDataBuilder;
-     * 手动创建报错 java.lang.NoClassDefFoundError: com/android/tools/idea/templates/ModuleTemplateDataBuilder
-     * 注意 com.android.tools.idea.npw.model.RenderTemplateModel.TemplateRenderer.renderTemplate 本身就写着 FIX ME
-     */
-    private fun RenderTemplateModel.renderTemplate(
-        layoutName: String,
-        rootTag: String,
-    ): File? {
-        val paths = template.get().paths
-        paths.moduleRoot ?: return null
-        val context = RenderingContext(
-            project = project,
-            module = module,
-            commandName = commandName,
-            templateData = this.moduleTemplateDataBuilder.build(),
-            moduleRoot = paths.moduleRoot!!,
-            dryRun = false,
-            showErrors = true
-        )
-        val executor = DefaultRecipeExecutor(context)
-        val resOut = context.moduleTemplateData?.resDir ?: return null
-        context.moduleTemplateData?.let {
-            executor.layoutResourceFileRecipe(it, layoutName, rootTag)
-            return resOut.resolve("layout/${layoutName}.xml")
-        } ?: return null
-    }
-
-    /**
-     * wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/other/files/layoutResourceFile/layoutResourceFileRecipe.kt
-     * com.android.tools.idea.wizard.template.impl.other.files.layoutResourceFile.layoutResourceFileTemplate 赋值 recipe
-     * 执行时调用 layoutResourceFileRecipe
-     * Recipe 定义于 wizard/template-plugin/src/com/android/tools/idea/wizard/template/Template.kt
-     * typealias Recipe = RecipeExecutor.(TemplateData) -> Unit
-     */
-    private fun RecipeExecutor.layoutResourceFileRecipe(
-        moduleData: ModuleTemplateData,
-        layoutName: String,
-        rootTag: String,
-    ) {
-        val resOut = moduleData.resDir
-        save(layoutXml(rootTag), moduleData.resDir.resolve("layout/${layoutName}.xml"))
-        open(resOut.resolve("layout/${layoutName}.xml"))
     }
 }
