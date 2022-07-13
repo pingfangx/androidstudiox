d2c7c155150c77180a76af960e936da3dbc1c8a5
pingfangx <pingfangx@pingfangx.com>
2022-07-13 11:39:35 +0800

    [templatex] action icon

---
+++ templatex/src/main/kotlin/com/pingfangx/plugin/templatex/icon/TemplateXIcons.kt
@@ -0,0 +1,14 @@
+package com.pingfangx.plugin.templatex.icon
+
+import com.intellij.openapi.util.IconLoader
+
+/**
+ * 图标
+ *
+ * @author pingfangx
+ * @date 2022/7/13
+ */
+object TemplateXIcons {
+    @JvmField
+    val Icon = IconLoader.getIcon("icons/icon.svg", javaClass)
+}
