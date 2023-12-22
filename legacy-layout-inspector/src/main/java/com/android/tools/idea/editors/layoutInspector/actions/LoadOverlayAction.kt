93718c75e4f565605fea66814a5109dc89deaf9b
pingfangx <pingfangx@pingfangx.com>
2023-12-22 11:33:36 +0800

    [legacy layout inspector] new icons for Layout Inspector

--- legacy-layout-inspector/src/main/java/com/android/tools/idea/editors/layoutInspector/actions/LoadOverlayAction.kt
+++ legacy-layout-inspector/src/main/java/com/android/tools/idea/editors/layoutInspector/actions/LoadOverlayAction.kt
@@ -31,17 +31,34 @@ import com.intellij.openapi.fileChooser.FileTypeDescriptor
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
-import icons.StudioIcons
 import java.awt.Image
 import java.io.IOException
 import javax.imageio.ImageIO
+import javax.swing.Icon
 import javax.swing.JComponent
 
+private fun getLayoutInspectorIcon(iconName: String): Icon? {
+  return try {
+    // Hedgehog
+    val clazz = Class.forName("icons.StudioIcons\$LayoutInspector")
+    val field = clazz.getField(iconName)
+    field.get(null) as? Icon
+  } catch (ignored: Throwable) {
+    // Iguana
+    try {
+      val clazz = Class.forName("icons.StudioIcons\$LayoutInspector\$Toolbar")
+      val field = clazz.getField(iconName)
+      field.get(null) as? Icon
+    } catch (ignored: Throwable) {
+      null
+    }
+  }
+}
 /**
  * Lets the user choose an image to overlay on top of the captured view to compare the app's visual against design mocks.
  */
 class LoadOverlayAction(private val myPreview: ViewNodeActiveDisplay) :
-    AnAction(ACTION_ID, "Overlay Image", StudioIcons.LayoutInspector.LOAD_OVERLAY), CustomComponentAction {
+    AnAction(ACTION_ID, "Overlay Image", getLayoutInspectorIcon("LOAD_OVERLAY")), CustomComponentAction {
   companion object {
     @JvmField
     val ACTION_ID = "Load Overlay"
@@ -58,11 +75,11 @@ class LoadOverlayAction(private val myPreview: ViewNodeActiveDisplay) :
     super.update(e)
     if (e == null) return
     if (myPreview.hasOverlay()) {
-      e.presentation.icon = StudioIcons.LayoutInspector.CLEAR_OVERLAY
+      e.presentation.icon = getLayoutInspectorIcon("CLEAR_OVERLAY")
       e.presentation.text = "Clear Overlay"
     }
     else {
-      e.presentation.icon = StudioIcons.LayoutInspector.LOAD_OVERLAY
+      e.presentation.icon = getLayoutInspectorIcon("LOAD_OVERLAY")
       e.presentation.text = ACTION_ID
     }
   }
